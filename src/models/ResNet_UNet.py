dec78983ace7f80b8371f7e7cfc70dadd2da9295
AsimGenki <asim@genki.team>
2025-07-02 14:12:56 +0200

    ..

--- src/models/ResNet_UNet.py
+++ src/models/ResNet_UNet.py
@@ -55,7 +55,7 @@ class ResNetEncoder(nn.Module):
     def __init__(self, in_channels=1):
         super().__init__()
         # Initial conv block
-        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=1, padding=3, bias=False)
         self.bn1   = nn.BatchNorm2d(64)
         self.relu  = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
